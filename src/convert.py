64322d85a14f0d0dd687fb5662e18bf0e188cb41
xiaohe <scofield.he907@gmail.com>
2017-08-30 12:22:16 +0800

    del all other dataset except 'Table Layout' & recalculate mData for global data

--- src/convert.py
+++ src/convert.py
@@ -16,43 +16,45 @@ data_type = np.dtype({
 
 
 def convert(raw_data, post_data):
-    for item in raw_data:
-        mlat, mlon = aacgmv2.convert(item["gdlat"], item["glon"], alt=350,
-                                     date=datetime.date(item["year"], item["month"], item["day"]), a2g=False)
-        mlt = aacgmv2.convert_mlt(mlon, datetime.datetime(item["year"], item["month"], item["day"],
-                                                          item["hour"], item["min"], item["sec"]), m2a=False)
+    """
+    calculate mlat, mlon and mlt for every data raw.
+    :param raw_data: dataset of raw hdf5 files
+    :param post_data: dataset conclude mlat,mlon and mlt
+    :return: post_data
+    """
+    start_time = time.time()
+    for raw in raw_data:
+        mlat, mlon = aacgmv2.convert(raw["gdlat"], raw["glon"], alt=350,
+                                     date=datetime.date(raw["year"], raw["month"], raw["day"]), a2g=False)
+        mlt = aacgmv2.convert_mlt(mlon, datetime.datetime(raw["year"], raw["month"], raw["day"],
+                                                          raw["hour"], raw["min"], raw["sec"]), m2a=False)
 
         a = np.array((mlat, mlon, mlt), dtype=data_type)
         post_data.append(a)
-    print("convert work done!")
+    else:
+        print("all aac items have been converted to m, "
+              "and length of aac = {}, length of m = {}".format(len(raw_data), len(post_data)))
+        print("it cost {}".format(round((time.time() - start_time), 2)))
+    return post_data
 
 
 file_path = "C:\\DATA\\GPS_MIT\\2013\\data\\"
-
 count = 0
 for fi in os.listdir(file_path):
     if ".hdf5" in fi:
         file = h5py.File(file_path + fi)
+        print("time = {}".format(datetime.datetime), "raw file = {}".format(file), "count = {}".format(count))
         count += 1
     else:
         continue
 
-    print(file)
-    for item in file:
-        print(item)
+    for item in file["Data"]:
+        if item != "Table Layout":
+            del file["Data"][item]
 
-    if "Post NA Data" in file["Data"]:
-        print("{} have 'Post NA Data' and will be deleted".format(file))
-        del file["Data"]["Post NA Data"]
-
-    start_time = time.time()
     raw_dat = file["Data"]["Table Layout"]
     post_dat = []
     convert(raw_dat, post_dat)
-    file["Data"].create_dataset("Post NA Data", data=post_dat, chunks=True)
-    print("len of converted data: --> {}".format(len(post_dat)))
-    print("len of pre-convert data: --> {}".format(len(raw_dat)))
-    print("{} convert work cost --> {}".format(fi, round(time.time() - start_time, 2)))
-    print("count = {}".format(count))
+    file["Data"].create_dataset("mData", data=post_dat, chunks=True)
 
-print("work done!")
+print("all files in {} done!".format(file_path))
