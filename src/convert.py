7622746ce58ae656a7b1443dee73c7c8ae76e0d9
xiaohe <scofield.he907@gmail.com>
2017-11-30 17:37:40 +0800

    optimize it, make the convert file run faster

--- src/convert.py
+++ src/convert.py
@@ -6,8 +6,7 @@ import time
 import datetime
 import numpy as np
 import h5py
-import aacgmv2
-
+from aacgmv2 import convert, convert_mlt
 
 data_type = np.dtype({
     "names": ['mlat', 'mlon', 'mlt'],
@@ -15,29 +14,38 @@ data_type = np.dtype({
 })
 
 
-def convert(raw_data, post_data):
+def gp2gm(raw_data, post_data):
     """
     calculate mlat, mlon and mlt for every data raw.
     :param raw_data: dataset of raw hdf5 files
     :param post_data: dataset conclude mlat,mlon and mlt
     :return: post_data
     """
-    # start_time = time.time()
-    for raw in raw_data:
-        mlat, mlon = aacgmv2.convert(raw["gdlat"], raw["glon"], alt=350,
-                                     date=datetime.date(raw["year"], raw["month"], raw["day"]), a2g=False)
-        mlt = aacgmv2.convert_mlt(mlon, datetime.datetime(raw["year"], raw["month"], raw["day"],
-                                                          raw["hour"], raw["min"], raw["sec"]), m2a=False)
+    glat, glon = raw_data["gdlat"], raw_data["glon"]
+    year, month, day = raw_data["year"], raw_data["month"], raw_data["day"]
+    hour, minute, sec = raw_data["hour"], raw_data["min"], raw_data["sec"]
+    for i in range(len(raw_data)):
+        mlat, mlon = convert(glat[i], glon[i], alt=350, date=datetime.date(year[i], month[i], day[i]), a2g=False)
+        mlt = convert_mlt(mlon, datetime.datetime(year[i], month[i], day[i], hour[i], minute[i], sec[i]), m2a=False)
+        a = np.array((mlat, mlon, mlt), dtype=data_type)
+        post_data.append(a)
+    """
+        for raw in raw_data:
+        mlat, mlon = convert(raw["gdlat"], raw["glon"], alt=350,
+                             date=datetime.date(raw["year"], raw["month"], raw["day"]), a2g=False)
+        mlt = convert_mlt(mlon, datetime.datetime(raw["year"], raw["month"], raw["day"],
+                                                  raw["hour"], raw["min"], raw["sec"]), m2a=False)
 
         a = np.array((mlat, mlon, mlt), dtype=data_type)
         post_data.append(a)
     else:
         print("length of aac = {}, length of cvt = {}".format(len(raw_data), len(post_data)))
         # print("it cost {}".format(round((time.time() - start_time), 2)))
+    """
     return post_data
 
 
-file_path = "C:\\DATA\\GPS_MIT\\2006\\data\\"
+file_path = "C:\\DATA\\GPS_MIT\\2011\\data\\"
 count = 0
 for fi in os.listdir(file_path):
     if ".hdf5" in fi:
@@ -53,8 +61,8 @@ for fi in os.listdir(file_path):
     time0 = time.time()
     raw_dat = file["Data"]["Table Layout"]
     cvt_data = []
-    convert(raw_dat, cvt_data)
-    file["Data"].create_dataset("mData", data=cvt_data, dtype=data_type, chunks=(391, ), compression="gzip")
-    print(datetime.datetime.now(), "converting of this file cost: {}".format(round(time.time() - time0, 2)))
+    gp2gm(raw_dat, cvt_data)
+    file["Data"].create_dataset("mData", data=cvt_data, dtype=data_type, chunks=(391,), compression="gzip")
+    print(datetime.datetime.now(), "gp2gm of this file cost: {}".format(round(time.time() - time0, 2)))
 
 print("all files in {} done!".format(file_path))
